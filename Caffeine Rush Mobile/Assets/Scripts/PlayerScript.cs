ad0c64b4b14e3ff508792d573ccd422aa0550c78
strike-freedom-95 <127664180+strike-freedom-95@users.noreply.github.com>
2024-10-26 00:37:46 +0530

    Backup Operation
    Clearing some space. So performing Data Backup

---
+++ Caffeine Rush Mobile/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    [SerializeField] Joystick joystick;
+
+    private void Start()
+    {
+        
+    }
+}
